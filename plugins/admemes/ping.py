1fdc5d3a89814186573bc4a757d2f4342233ba72
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-06-18 18:49:43 +0530

    Update ping.py

--- plugins/admemes/ping.py
+++ plugins/admemes/ping.py
@@ -9,7 +9,37 @@ from plugins.helper_functions.cust_p_filters import f_onw_fliter
 
 # -- Constants -- #
 ALIVE = "You are not dead. You are still here. You have no love for me now. Okay .. you're not changed like you used to be..🙂" 
-HOW_TO_OWN = "<b>ᴏᴡɴᴇʀ ›› ʜᴇʏ ᴛʜᴀɴᴋs ғᴏʀ ᴜsɪɴɢ ᴏᴜʀ ʙᴏᴛ ᴄᴏɴᴛᴀᴄᴛ ᴍᴇ ʙᴇʟᴏᴡ</b>"
+HOW_TO_OWN = "<b>ᴏᴡɴᴇʀ ›› Auto + IMDB + Unlimited Filter Bot. 🥰🥰
+
+An advanced Filter Bot with Unlimited filters, also Support Auto filters and inline Media search Video and File Store etc...
+
+Features :-
+
+›› Nearly unlimited filter
+›› Custom IMDb Poster + Details
+›› Double Buttons (callbacks)
+›› Broadcast 
+›› Random Start Pics
+›› Inline Media Search
+›› Index Files (no user session required)
+›› Extra Show Commands (chats, users, disable, enable, invite. etc
+›› Force Subscribe
+›› Search on imdb
+›› Supports all type of filters
+›› Can save button filters directly (Rose Bot Feature)
+›› Supports multiple PM connections
+›› And all other features of a Filter Bot and Media Search Bot 
+›› File Store
+
+How to Deploy On Heroku! :
+
+🔗 Tutorial : https://youtu.be/MfUjmZ1mpfc
+
+Dont Forgot To Subscribe ❤️
+
+Deploy Tutorial  ›› https://youtu.be/MfUjmZ1mpfc
+
+A Big Credits To ➡ Adhi</b>"
 CHANNEL = "<b>𝚈𝙾𝚄𝚃𝚄𝙱𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻</b> ›› https://youtube.com/channel/UCMzFIpsfTkZfkI-O20o1gww\n\n<b>𝚄𝙿𝙳𝙰𝚃𝙴𝚂 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ›› https://t.me/cynitebots</b>\n\n<b>𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ›› https://t.me/cynitemovies</b>"
 ZSEARCHERBOT = "<b>𝙱𝙾𝚃 ›› https://t.me/zsearcherbot</b>"
 # -- Constants End -- #
