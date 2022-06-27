3f7a31ac6320843e5303489dc317781ad8270641
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-06-27 21:25:18 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -72,7 +72,7 @@ Note:- Give a user details
 3. /Runs - 𝚂𝙾𝙼𝙴 𝚁𝙰𝙽𝙳𝙾𝙼 𝙳𝙸𝙰𝙻𝙾𝙶𝚄𝙴𝚂 
 4. /Goal or /Shoot - 𝚃𝙾 𝙼𝙰𝙺𝙴 𝙰 𝙶𝙾𝙰𝙻 𝙾𝚁 𝚂𝙷𝙾𝙾𝚃
 5. /luck or /cownd - 𝚂𝙿𝙸𝙽 𝙰𝙽𝙳 𝚃𝚁𝚈 𝚈𝙾𝚄𝚁 𝙻𝚄𝙲𝙺"""
-    DEPLOY_TXT = """<b>Thanks For Using</b> 
+    DEPLOY_TXT = """<b>ᴛʜᴀɴᴋs ғᴏʀ ᴜsɪɴɢ</b> 
   
 <b>✮ Subscribe››</b> <i><b>https://youtube.com/channel/UCMzFIpsfTkZfkI-O20o1gww</b></i>
 
