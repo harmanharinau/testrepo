2662489d88de05fbbd5ccc101dda0f97f407784b
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-06-27 21:01:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -39,13 +39,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-        ], [
-            InlineKeyboardButton('🔍sᴇᴀʀᴄʜ', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖ᴜᴘᴅᴀᴛᴇs', url='https://t.me/Cynitebots')
-        ], [
-            InlineKeyboardButton('ℹ️ʜᴇʟᴘ', callback_data='help'),
-            InlineKeyboardButton('🔰ᴀʙᴏᴜᴛ', callback_data='about')
+            InlineKeyboardButton('🎁ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ sᴜʀᴘʀɪsᴇ🎁', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ]]         
         reply_markup = InlineKeyboardMarkup(buttons)        
         await message.reply_photo(
