e116b53acbc3cf4d0b6072b76a5b565bc9a77d2a
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-06-28 12:49:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -355,7 +355,7 @@ async def delete_all_index(bot, message):
                     )
                 ]
             ]
-   
+       ) 
 @Client.on_callback_query(filters.regex(r'^autofilter_delete'))
 async def delete_all_index_confirm(bot, message):
     await Media.collection.drop()
